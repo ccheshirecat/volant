c4ca16f3b295e359387a67a5eccd0dbb846877df
mx <spuncheshirecat@gmail.com>
2025-10-09 20:22:12 +0700

    Add Drift L4 switch integration

--- internal/drift/dataplane/manager_linux.go
+++ internal/drift/dataplane/manager_linux.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //go:build linux
 
 package dataplane
@@ -177,3 +178,185 @@ func protoName(proto uint8) string {
 		return fmt.Sprintf("%d", proto)
 	}
 }
+||||||| parent of 1682feb (Add Drift L4 switch integration)
+=======
+//go:build linux
+
+package dataplane
+
+import (
+	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net"
+	"sync"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+)
+
+type manager struct {
+	logger   *slog.Logger
+	program  *ebpf.Program
+	portmap  *ebpf.Map
+	link     link.Link
+	iface    string
+	mu       sync.Mutex
+	closed   bool
+	programs *ebpf.Collection
+}
+
+func newManager(opts Options) (Interface, error) {
+	if opts.Logger == nil {
+		opts.Logger = slog.Default()
+	}
+	if opts.ObjectPath == "" {
+		return nil, fmt.Errorf("dataplane: bpf object path required")
+	}
+	if opts.Interface == "" {
+		return nil, fmt.Errorf("dataplane: interface name required")
+	}
+
+	spec, err := ebpf.LoadCollectionSpec(opts.ObjectPath)
+	if err != nil {
+		return nil, fmt.Errorf("dataplane: load collection spec: %w", err)
+	}
+
+	coll, err := ebpf.NewCollection(spec)
+	if err != nil {
+		return nil, fmt.Errorf("dataplane: create collection: %w", err)
+	}
+
+	prog, ok := coll.Programs["drift_l4_ingress"]
+	if !ok {
+		coll.Close()
+		return nil, errors.New("dataplane: program drift_l4_ingress not found")
+	}
+
+	portmap, ok := coll.Maps["portmap"]
+	if !ok {
+		coll.Close()
+		return nil, errors.New("dataplane: portmap not found")
+	}
+
+	iface, err := net.InterfaceByName(opts.Interface)
+	if err != nil {
+		coll.Close()
+		return nil, fmt.Errorf("dataplane: lookup interface %s: %w", opts.Interface, err)
+	}
+
+	l, err := link.AttachTCX(link.TCXOptions{
+		Program:   prog,
+		Interface: iface.Index,
+		Attach:    ebpf.AttachTCXIngress,
+	})
+	if err != nil {
+		coll.Close()
+		return nil, fmt.Errorf("dataplane: attach tc: %w", err)
+	}
+
+	return &manager{
+		logger:   opts.Logger.With("component", "dataplane"),
+		program:  prog,
+		portmap:  portmap,
+		link:     l,
+		iface:    opts.Interface,
+		programs: coll,
+	}, nil
+}
+
+func (m *manager) ApplyBridge(_ context.Context, proto uint8, hostPort uint16, destIP net.IP, destPort uint16) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.closed {
+		return errors.New("dataplane: manager closed")
+	}
+
+	ip4 := destIP.To4()
+	if ip4 == nil {
+		return fmt.Errorf("dataplane: destination ip %s not ipv4", destIP)
+	}
+
+	key := portmapKey{
+		Proto: proto,
+		Port:  htons(hostPort),
+	}
+
+	value := portmapValue{
+		DestIP:   binary.BigEndian.Uint32(ip4),
+		DestPort: htons(destPort),
+	}
+
+	if err := m.portmap.Put(&key, &value); err != nil {
+		return fmt.Errorf("dataplane: portmap update: %w", err)
+	}
+
+	m.logger.Info("route applied", "proto", protoName(proto), "host_port", hostPort, "dest_ip", destIP.String(), "dest_port", destPort)
+	return nil
+}
+
+func (m *manager) Remove(_ context.Context, proto uint8, hostPort uint16) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.closed {
+		return errors.New("dataplane: manager closed")
+	}
+
+	key := portmapKey{
+		Proto: proto,
+		Port:  htons(hostPort),
+	}
+
+	if err := m.portmap.Delete(&key); err != nil && !errors.Is(err, ebpf.ErrKeyNotExist) {
+		return fmt.Errorf("dataplane: portmap delete: %w", err)
+	}
+
+	m.logger.Info("route removed", "proto", protoName(proto), "host_port", hostPort)
+	return nil
+}
+
+func (m *manager) Close() error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.closed {
+		return nil
+	}
+	m.closed = true
+	if m.link != nil {
+		_ = m.link.Close()
+	}
+	if m.programs != nil {
+		m.programs.Close()
+	}
+	return nil
+}
+
+type portmapKey struct {
+	Proto uint8
+	_     uint8
+	Port  uint16
+}
+
+type portmapValue struct {
+	DestIP   uint32
+	DestPort uint16
+	_        uint16
+}
+
+func htons(value uint16) uint16 {
+	return value<<8 | value>>8
+}
+
+func protoName(proto uint8) string {
+	switch proto {
+	case 6:
+		return "tcp"
+	case 17:
+		return "udp"
+	default:
+		return fmt.Sprintf("%d", proto)
+	}
+}
+>>>>>>> 1682feb (Add Drift L4 switch integration)
--- internal/drift/vsockproxy/manager_linux.go
+++ internal/drift/vsockproxy/manager_linux.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //go:build linux
 
 package vsockproxy
@@ -172,3 +173,209 @@ func (p *proxy) stop() {
 	<-p.done
 	p.logger.Info("vsock proxy stopped")
 }
+||||||| parent of 1682feb (Add Drift L4 switch integration)
+=======
+//go:build linux
+
+package vsockproxy
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net"
+	"sync"
+
+	"github.com/mdlayher/vsock"
+)
+
+type manager struct {
+	logger   *slog.Logger
+	bindAddr string
+	mu       sync.Mutex
+	proxies  map[string]*proxy
+	closed   bool
+}
+
+type proxy struct {
+	proto     string
+	hostPort  uint16
+	cid       uint32
+	guestPort uint16
+	listener  net.Listener
+	cancel    context.CancelFunc
+	done      chan struct{}
+	logger    *slog.Logger
+}
+
+func newManager(opts Options) (Manager, error) {
+	logger := opts.Logger
+	if logger == nil {
+		logger = slog.Default()
+	}
+	bind := opts.BindAddress
+	if bind == "" {
+		bind = "0.0.0.0"
+	}
+
+	return &manager{
+		logger:   logger.With("component", "vsockproxy"),
+		bindAddr: bind,
+		proxies:  make(map[string]*proxy),
+	}, nil
+}
+
+func (m *manager) key(proto string, port uint16) string {
+	return fmt.Sprintf("%s/%d", proto, port)
+}
+
+func (m *manager) Upsert(ctx context.Context, proto string, hostPort uint16, cid uint32, guestPort uint16) error {
+	if proto != "tcp" {
+		return fmt.Errorf("vsock proxy: protocol %q not supported", proto)
+	}
+
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.closed {
+		return errors.New("vsock proxy: manager closed")
+	}
+
+	key := m.key(proto, hostPort)
+	if existing, ok := m.proxies[key]; ok {
+		existing.stop()
+	}
+
+	addr := fmt.Sprintf("%s:%d", m.bindAddr, hostPort)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("vsock proxy: listen %s: %w", addr, err)
+	}
+
+	childCtx, cancel := context.WithCancel(context.Background())
+	px := &proxy{
+		proto:     proto,
+		hostPort:  hostPort,
+		cid:       cid,
+		guestPort: guestPort,
+		listener:  listener,
+		cancel:    cancel,
+		done:      make(chan struct{}),
+		logger:    m.logger.With("host_port", hostPort, "cid", cid, "guest_port", guestPort),
+	}
+	px.start(childCtx)
+	m.proxies[key] = px
+	return nil
+}
+
+func (m *manager) Remove(ctx context.Context, proto string, hostPort uint16) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	key := m.key(proto, hostPort)
+	proxy, ok := m.proxies[key]
+	if !ok {
+		return nil
+	}
+	proxy.stop()
+	delete(m.proxies, key)
+	return nil
+}
+
+func (m *manager) Close() error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.closed {
+		return nil
+	}
+	m.closed = true
+	for key, proxy := range m.proxies {
+		proxy.stop()
+		delete(m.proxies, key)
+	}
+	return nil
+}
+
+func (p *proxy) start(ctx context.Context) {
+	go func() {
+		p.logger.Info("vsock proxy started")
+		defer close(p.done)
+		for {
+			conn, err := p.listener.Accept()
+			if err != nil {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+				nErr, ok := err.(net.Error)
+				if ok && nErr.Temporary() {
+					p.logger.Warn("temporary accept error", "error", err)
+					continue
+				}
+				p.logger.Error("accept error", "error", err)
+				return
+			}
+			go p.handleConnection(ctx, conn)
+		}
+	}()
+}
+
+func (p *proxy) handleConnection(ctx context.Context, conn net.Conn) {
+	defer conn.Close()
+	vsockConn, err := dialVsock(ctx, p.cid, uint32(p.guestPort))
+	if err != nil {
+		p.logger.Error("vsock dial failed", "error", err)
+		return
+	}
+	defer vsockConn.Close()
+
+	var wg sync.WaitGroup
+	copyStream := func(dst io.Writer, src io.Reader) {
+		defer wg.Done()
+		if _, err := io.Copy(dst, src); err != nil {
+			p.logger.Warn("copy stream", "error", err)
+		}
+	}
+	wg.Add(2)
+	go copyStream(vsockConn, conn)
+	go copyStream(conn, vsockConn)
+	wg.Wait()
+}
+
+func (p *proxy) stop() {
+	p.cancel()
+	_ = p.listener.Close()
+	<-p.done
+	p.logger.Info("vsock proxy stopped")
+}
+
+func dialVsock(ctx context.Context, cid uint32, port uint32) (*vsock.Conn, error) {
+	type result struct {
+		conn *vsock.Conn
+		err  error
+	}
+
+	ch := make(chan result, 1)
+	go func() {
+		conn, err := vsock.Dial(cid, port, nil)
+		ch <- result{conn: conn, err: err}
+	}()
+
+	select {
+	case <-ctx.Done():
+		go func() {
+			res := <-ch
+			if res.err == nil {
+				_ = res.conn.Close()
+			}
+		}()
+		return nil, ctx.Err()
+	case res := <-ch:
+		if res.err != nil {
+			return nil, res.err
+		}
+		return res.conn, nil
+	}
+}
+>>>>>>> 1682feb (Add Drift L4 switch integration)
